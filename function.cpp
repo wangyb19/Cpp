ee1f328e5662cfaee2e1427347bfe53a71ca9097
wangyb19 <wangyb19@lzu.edu.cn>
2022-11-28 22:23:04 +0800

    using function

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+
+int main()
+{
+using namespace std;
+
+double a,l;
+
+cout << "Please input the value of are of a square: ";
+cout << endl;
+cin >> a;
+l=sqrt(a);
+cout << "The area of the squrae is: " << a <<endl;
+cout << "The length of the square is: " << l << endl;
+return 0;
+}
