3bc541a4367da15b4a7bb1819877391156240cce
wangyb19 <wangyb19@lzu.edu.cn>
2022-11-29 20:11:11 +0800

    for loop in C++

---
+++ forloop.cpp
@@ -0,0 +1,14 @@
+//The purpose of this program is illustrating 
+//the for loop in the C++
+
+#include<iostream>
+
+int main()
+{
+using namespace std;
+int i;
+for (i=0;i<=5;i++)
+	cout << "The " << i << "-th " << "loop" << endl;
+cout << "Over!" << endl;
+return 0;
+}
